00b57c6c9e53f7bf7ba875f1c06678d61697865d
Jorgegga <65369692+Jorgegga@users.noreply.github.com>
2022-02-26 23:37:14 +0100

    maps

--- app/src/main/java/com/example/practicafinal2trimestre_jorgegonzalogalindoalmena/InicioActivity.kt
+++ app/src/main/java/com/example/practicafinal2trimestre_jorgegonzalogalindoalmena/InicioActivity.kt
@@ -8,20 +8,33 @@ import android.view.View
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import com.example.practicafinal2trimestre_jorgegonzalogalindoalmena.databinding.ActivityInicioBinding
 import com.example.practicafinal2trimestre_jorgegonzalogalindoalmena.fullscreen.Principal
+import com.example.practicafinal2trimestre_jorgegonzalogalindoalmena.maps.MapsFragment
 import com.google.android.material.navigation.NavigationView
 
 
-class InicioActivity : Principal(), NavigationView.OnNavigationItemSelectedListener  {
+class InicioActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener  {
     lateinit var binding : ActivityInicioBinding
+    lateinit var transaction : FragmentTransaction
+    lateinit var fragmentPortada : Fragment
+    lateinit var fragmentMaps : Fragment
+    lateinit var fragmentWeb : Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityInicioBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setToolbar()
-        pantallaCompleta()
+        title="Scarlet Perception"
+        fragmentPortada = PortadaFragment()
+        fragmentMaps = MapsFragment()
+
+        supportFragmentManager.beginTransaction().add(R.id.fragmentContainerView, fragmentPortada).commit()
     }
+
 
     fun setToolbar(){
         val toolbar: Toolbar = binding.mainToolbar
@@ -43,7 +56,21 @@ class InicioActivity : Principal(), NavigationView.OnNavigationItemSelectedListe
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        return false
+        transaction = supportFragmentManager.beginTransaction()
+        when(item.itemId){
+            R.id.btnInicio -> {
+                transaction.replace(R.id.fragmentContainerView, fragmentPortada).commit()
+                return true
+            }
+
+            R.id.btnMaps ->{
+                transaction.replace(R.id.fragmentContainerView, fragmentMaps).commit()
+                return true
+            }
+            else ->{
+                return false
+            }
+        }
     }
 
 }
---
+++ app/src/main/java/com/example/practicafinal2trimestre_jorgegonzalogalindoalmena/maps/MapsFragment.kt
@@ -0,0 +1,170 @@
+package com.example.practicafinal2trimestre_jorgegonzalogalindoalmena.maps
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import com.example.practicafinal2trimestre_jorgegonzalogalindoalmena.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+
+class MapsFragment : Fragment(), OnMapReadyCallback {
+    private lateinit var mMap: GoogleMap
+    val PERMISO_CODE = 100
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
+        // Add a marker in Sydney and move the camera
+        val tienda = LatLng(36.838600, -2.465152)
+        mMap.addMarker(MarkerOptions().position(tienda).title("Localizacion de la tienda de musica"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tienda))
+        enableLocation()
+        mMap.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(tienda, 15f), 4000, null
+        )
+
+    }
+
+    private fun enableLocation() {
+        if(!::mMap.isInitialized) return
+        if(isPermisosConcedidos()){
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            mMap.isMyLocationEnabled = true
+        }else{
+            pedirPermisos()
+        }
+    }
+
+    private fun isPermisosConcedidos(): Boolean {
+        return (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun pedirPermisos(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_COARSE_LOCATION) || ActivityCompat.shouldShowRequestPermissionRationale(requireActivity(), Manifest.permission.ACCESS_FINE_LOCATION)){
+            Toast.makeText(requireContext(), "Debe activar los permisos", Toast.LENGTH_SHORT).show()
+        }else{
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION, Manifest.permission.ACCESS_FINE_LOCATION), PERMISO_CODE)
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if(!isPermisosConcedidos()){
+            if(!::mMap.isInitialized) {
+                return
+            }
+            if (ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                    requireContext(),
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+                return
+            }
+            mMap.isMyLocationEnabled = false
+            Toast.makeText(requireContext(), "Permisos necesarios para la app, activelos", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISO_CODE->{
+                if(grantResults.isNotEmpty() && (grantResults[0] == PackageManager.PERMISSION_GRANTED && grantResults[1] == PackageManager.PERMISSION_GRANTED)){
+                    if (ActivityCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                            requireContext(),
+                            Manifest.permission.ACCESS_COARSE_LOCATION
+                        ) != PackageManager.PERMISSION_GRANTED
+                    ) {
+                        // TODO: Consider calling
+                        //    ActivityCompat#requestPermissions
+                        // here to request the missing permissions, and then overriding
+                        //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                        //                                          int[] grantResults)
+                        // to handle the case where the user grants the permission. See the documentation
+                        // for ActivityCompat#requestPermissions for more details.
+                        return
+                    }
+                    mMap.isMyLocationEnabled = true
+                }else{
+                    Toast.makeText(requireContext(), "Has rechazado los permisos", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        var rootView = inflater.inflate(R.layout.fragment_maps, container, false)
+        var mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        return rootView
+    }
+
+    companion object {
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(): MapsFragment{
+            return MapsFragment()
+        }
+
+    }
+
+
+}
